e293cd255ec3f14ec7a88622bcf629bc8639f1d7
Santez <artursantez@gmail.com>
2025-01-10 15:07:15 -0300

    Exercício 8

---
+++ JS/Exercicios/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+
+console.log(`Nacionalidade: ${país}`)
+
+if (país == 'Brasil') { //Condição Composta
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
