100946524641c1e2d32e84c69056855d27f3ed8e
Santez <artursantez@gmail.com>
2025-01-16 15:59:45 -0300

    Exercício 16 corrigido

--- JS/Exercicios/aula014ex/ex016/script.js
+++ JS/Exercicios/aula014ex/ex016/script.js
@@ -8,25 +8,27 @@ function contar(){
     var fim = window.document.getElementById('fim')
     var pas = window.document.getElementById('passo')
 
-    if (ini.value.trim() == '' || fim.value.trim() == '') {
+    if (ini.value.trim() == '' || fim.value.trim() == '' || pas.value.trim() == '') {
         window.alert('Por favor, insira os valores')
-    } else if (pas.value.trim() == '' || Number(pas.value) <= 0){
-        window.alert(`Passo inválido ou não inserido, será considerado passo 1`)
+    } else if (Number(pas.value) <= 0){
+        window.alert(`Passo inválido, será considerado passo 1`)
         resul.innerHTML = ''
         pas.value = 1
         for (var c = Number(ini.value); c <= Number(fim.value); c += Number(pas.value)) {
-            resul.innerHTML += c !== Number(fim.value) ? `${c} &#x1F449;` : `${c}&#x1F6A9`
+            resul.innerHTML += `${c} &#x1F449;`
         } 
         for (var c = Number(ini.value); c >= Number(fim.value); c -= Number(pas.value)) {
-            resul.innerHTML += c !== Number(fim.value) ? `${c} &#x1F449;` : `${c} &#x1F6A9` 
+            resul.innerHTML +=  `${c} &#x1F449;`
         }
+        resul.innerHTML += `&#x1F6A9`
     } else {
         resul.innerHTML = ''
         for (var c = Number(ini.value); c <= Number(fim.value); c += Number(pas.value)) {
-            resul.innerHTML += c !== Number(fim.value) ? `${c} &#x1F449;` : `${c}&#x1F6A9`
+            resul.innerHTML += `${c} &#x1F449;`
         } 
         for (var c = Number(ini.value); c >= Number(fim.value); c -= Number(pas.value)) {
-            resul.innerHTML += c !== Number(fim.value) ? `${c} &#x1F449;` : `${c} &#x1F6A9` 
+            resul.innerHTML += `${c} &#x1F449;`
         }
+        resul.innerHTML += `&#x1F6A9`
     }    
 }
