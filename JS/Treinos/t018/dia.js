2bb209ced288fa5f34f5669719a879173ba9bd16
Santez <artursantez@gmail.com>
2025-01-25 23:49:19 -0300

    Treino 18, correção

--- JS/Treinos/t018/dia.js
+++ JS/Treinos/t018/dia.js
@@ -1,47 +1,8 @@
 let date = new Date()
-let data = {dis: date.getDay(), dta: date.getDate(), mes: date.getMonth(), ano: date.getFullYear()}
-
-let stx = window.document.querySelector('p#data')
-let dtx = window.document.querySelector('p#semana')
-
-switch (data.mes) {
-    case 0:
-        data.mes = 1
-        break
-    case 1:
-        date.mes = 2
-        break
-    case 2:
-        date.mes = 3
-        break
-    case 3: 
-        date.mes = 4
-        break
-    case 4:
-        date.mes = 5
-        break
-    case 5:
-        date.mes = 6
-        break
-    case 6:
-        date.mes = 7
-        break
-    case 7:
-        date.mes = 8
-        break
-    case 8:
-        date.mes = 9
-        break
-    case 9:
-        date.mes = 10
-        break
-    case 10:
-        date.mes = 11
-        break
-    case 11:
-        date.mes = 12
-        break
-}
+let data = {dis: date.getDay(), 
+    dta: date.getDate(), 
+    mes: date.getMonth() + 1, 
+    ano: date.getFullYear()}
 
 if (data.dta < 10) {
     data.dta = '0' + data.dta
@@ -74,6 +35,9 @@ switch (data.dis) {
         data.dis = 'Sábado'
         break
 }
+
+let stx = window.document.querySelector('p#data')
+let dtx = window.document.querySelector('p#semana')
 
 stx.innerHTML = `${data.dta}/${data.mes}/${data.ano}`
 
